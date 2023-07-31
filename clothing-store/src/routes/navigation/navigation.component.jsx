17dd517de45c5b50b1b07e22ee34baf974b79eb7
Vukipoo <perovicv515@gmail.com>
2023-07-31 15:36:40 -0700

    update routes, add new compoenent ( navigation ), implement Fragment and Link in navigation component

---
+++ clothing-store/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+
+const Navigation = () => {
+    return (
+      <Fragment>
+        <div className='navigation'>
+            <div>Logo</div>
+            <div className='nav-links-container'>
+                <Link className='nav-link' to='/shop'>
+                    SHOP
+                </Link>
+            </div>
+          <h1>I am the navigation bar</h1>
+        </div>
+        <Outlet />
+      </Fragment>
+  
+    )
+  
+  }
+
+
+  export default Navigation;
