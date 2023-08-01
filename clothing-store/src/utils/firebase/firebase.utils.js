4e1dcd6ee49aa6c1869acce4ddc2952b69306ce5
Vukipoo <perovicv515@gmail.com>
2023-07-31 19:28:55 -0700

    create new component (sign-up-form) and implement the use of state

--- clothing-store/src/utils/firebase/firebase.utils.js
+++ clothing-store/src/utils/firebase/firebase.utils.js
@@ -6,6 +6,15 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 
+import {  
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from "firebase/firestore";
+import { useRevalidator } from "react-router-dom";
+import CategoryItem from "../../components/category-item/category-item.component";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAwctKC-F8t5E3bBQ34Lnw2bqZyNZ7CgPE",
   authDomain: "crazycat-clothing-db.firebaseapp.com",
@@ -19,8 +28,42 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: "select_account" });
 
 export const auth = getAuth(firebaseApp);
-export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore(firebaseApp);
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, "users", userAuth.uid);
+
+    console.log(userDocRef)
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    console.log(userSnapshot)
+    console.log(userSnapshot.exists())
+
+
+
+if(!userSnapshot.exists()) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+        await setDoc(userDocRef, {
+            displayName,
+            email,
+            createdAt
+            })
+    } catch (error) {
+        console.log('error creating user', error.message)
+    }
+}
+
+return userDocRef;
+
+}
